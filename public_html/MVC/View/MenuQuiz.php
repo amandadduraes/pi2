65dbca0c5b30b87d93cab930d763ff40e861c2f1
Ricardo50-dev <80547906+Ricardo50-dev@users.noreply.github.com>
2021-04-03 19:32:09 -0300

    MenuQuiz

---
+++ public_html/MVC/View/MenuQuiz.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Bibliotecas/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../Assets/css/MenuQuiz.css">
+    <link rel="stylesheet" href="../../Bibliotecas/Font-awesome/css/font-awesome.min.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <script scr="../../Bibliotecas/bootstrap-4.5.3-dist/js/bootstrap.min.js"></script>
+    <script scr="../../Bibliotecas/jquery/jquery.min.js"></script>
+    <div class="container-fluid d-flex p-0 col-8">
+        <div class="sidebar-container">
+            <div class="sidebar-logo">
+                DO 0 AO 1
+            </div>
+            <ul class="sidebar-navigation">
+                <li class="header">Navegação</li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-video-camera" aria-hidden="true"></i> Material em Video
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-book" aria-hidden="true"></i> Material em Leitura
+                    </a>
+                </li>
+                <li>
+                    <a href="MenuQuiz.php">
+                        <i class="fa fa-clock-o" aria-hidden="true"></i> Quizz
+                    </a>
+                </li>
+                <li>
+                    <a href="EditaQuiz.php">
+                        <i class="fa fa-superpowers" aria-hidden="true"></i> Cria Quizz
+                    </a>
+                </li>
+                <li class="header">Perfil</li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-id-card-o" aria-hidden="true"></i> Perfil
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-trophy" aria-hidden="true"></i> Ranking
+                    </a>
+                </li>
+                <li>
+                    <a href="EditaPerfil.php">
+                        <i class="fa fa-cogs" aria-hidden="true"></i> Editar Perfil
+                    </a>
+                </li>
+                <li class="header">Conect</li>
+                <li>
+                    <a href="login.php">
+                        <i class="fa fa-sign-in" aria-hidden="true"></i> Login
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="container">
+    <div class="row">
+        <div class="one-third col-md-4"> <a href="#" class="img-caption">
+                <figure> <img src="../../Assets/img/poo.jpg" alt="" />
+                    <figcaption>
+                        <h3>Orintada a Objetos</h3> <span>Teste seus conhecimentos em POO!</span>
+                    </figcaption>
+                </figure>
+            </a> </div>
+        <div class="one-third col-md-4"> <a href="#" class="img-caption">
+                <figure> <img src="../../Assets/img/c.png" alt="" />
+                    <figcaption>
+                        <h3>C</h3> <span>Teste seus conhecimentos<br></br> na linguagem C!</span>
+                    </figcaption>
+                </figure>
+            </a> </div>
+        <div class="one-third col-md-4"> <a href="#" class="img-caption">
+                <figure> <img src="../../Assets/img/cmais.png" alt="" />
+                    <figcaption>
+                        <h3>C++</h3> <span>Teste seus conhecimentos<br></br> na linguagem C++!</span>
+                    </figcaption>
+                </figure>
+            </a> </div>
+    </div>
+    <div class="row">
+        <div class="one-third col-md-4"> <a href="#" class="img-caption">
+                <figure> <img src="../../Assets/img/pi.jpg" alt="" />
+                    <figcaption>
+                        <h3>Programação Para Internet</h3> <span>Aprimore seus conhecimentos em html,<br></br> css, js, bootstrap e muito mais!</span>
+                    </figcaption>
+                </figure>
+            </a> </div>
+        <div class="one-third col-md-4"> <a href="#" class="img-caption">
+                <figure> <img src="../../Assets/img/py.png" alt="" />
+                    <figcaption>
+                        <h3>Python</h3> <span>Teste seus conhecimentos<br></br> na linguagem Python!</span>
+                    </figcaption>
+                </figure>
+            </a> </div>
+        <div class="one-third col-md-4"> <a href="#" class="img-caption">
+                <figure> <img src="../../Assets/img/calculo.png" alt="" />
+                    <figcaption>
+                        <h3>Cálculo</h3> <span>Teste seus conhecimentos em cálculo!</span>
+                    </figcaption>
+                </figure>
+            </a> </div>
+    </div>
+</div>
+    </div>
+</body>
+
+</html>
