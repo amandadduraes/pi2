dc7fb18cd80265a7cf10c29a76d9a5d14d56a218
Ricardo50-dev <80547906+Ricardo50-dev@users.noreply.github.com>
2021-04-24 14:52:51 -0300

    Criando navBar.php

--- public_html/controller/Quiz.php
+++ public_html/controller/Quiz.php
@@ -3,20 +3,17 @@
 
     $arrdeveter = [
         "pergunta",
-        "setA",
-        "setB",
-        "setC",
-        "setD",
         "questaoA",
         "questaoB",
         "questaoC",
         "questaoD",
         "descricao",
+        "correta",
         "tipo",
         "turma",
         "acao"
     ];
-    //print_r($_POST);
+    print_r($_POST);
     $erro = false;
     foreach($arrdeveter as $dev){
         if(!isset($_POST[$dev])){
@@ -31,19 +28,54 @@
             $desc_atv = $_POST["descricao"];
             $tipo = $_POST["tipo"];
             $turma = $_POST["turma"];
-            echo "$desc_atv--$turma--$tipo";
             criaAtividade($desc_atv, $turma, $tipo);
 
-            $pergunta=$_POST["pergunta"];
-            $questaoA = $_POST["questaoA"];
-            $questaoB = $_POST["questaoB"];
-            $questaoC = $_POST["questaoC"];
-            $questaoD = $_POST["questaoD"];
+            $contaTam=$_POST["pergunta"];
             
-            $setA=$_POST["setA"];
-            $setB=$_POST["setB"];
-            $setC=$_POST["setC"];
-            $setD=$_POST["setD"];
+
+            for($i = 0; $i < count($contaTam); $i++){
+                $alternativasCorretas = $_POST["correta"][$i];
+                $perguntas=$_POST["pergunta"][$i];
+
+                $alternativasA = $_POST["questaoA"][$i];
+                $alternativasB = $_POST["questaoB"][$i];
+                $alternativasC = $_POST["questaoC"][$i];
+                $alternativasD = $_POST["questaoD"][$i];
+
+                if($alternativasCorretas == '1'){
+                    $alt1 = "certo";
+                    $alt2 = "errado";
+                    $alt3 = "errado";
+                    $alt4 = "errado";
+                }else if($alternativasCorretas == '2'){
+                    $alt1 = "errado";
+                    $alt2 = "certo";
+                    $alt3 = "errado";
+                    $alt4 = "errado";
+                }else if($alternativasCorretas == '3'){
+                    $alt1 = "errado";
+                    $alt2 = "errado";
+                    $alt3 = "certo";
+                    $alt4 = "errado";
+                }else{
+                    $alt1 = "errado";
+                    $alt2 = "errado";
+                    $alt3 = "errado";
+                    $alt4 = "certo";
+                }
+                include("../model/Conexao.php");
+                $stmt = $PDO->query("select id from atividade where descricao='$desc_atv'");
+                $id_atv = $stmt->fetch(); 
+                echo "$id_atv['id']"; 
+            }
+            
+            
+            
+            
+            
+            echo "$alt1--$alt2--$alt3--$alt4";
+            //criaQuestaoEAlternativas($pergunta, $questaoA, $questaoB, $questaoC, $questaoD, $alt1, $alt2, $alt3, $alt4);
+            
         
         
 
